aadce2b92d0c6b6df1ec5d40d77b66c8e80b89a6
Arfan Anulal <151611444+ArfanAnulal@users.noreply.github.com>
2024-09-16 15:27:33 +0530

    Update 4Account.py

--- 4Account.py
+++ 4Account.py
@@ -1,3 +1,4 @@
+#Password in line 13
 #This file is for the details of account holder
 import pickle
 import os
@@ -9,7 +10,7 @@ def convertTuple(idtup):
 
 
 import mysql.connector as mc
-mycon=mc.connect(host='localhost', user='root',password='nafraJ20',database='email_subsystem') #mysql connection
+mycon=mc.connect(host='localhost', user='root',password='<YOURPASSWORD>',database='email_subsystem') #mysql connection
 
 
 f=open("idstoringfile.dat", "rb")
